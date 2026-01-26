146d83020250b81fca300e9d1eed36b87b2b7520
Mateoro1705 <mateorodriguez1705@yahoo.com>
2026-01-25 19:45:34 -0500

    Agregando una parte de responsividad

--- JS/scriptHeader.js
+++ JS/scriptHeader.js
@@ -1,11 +1,5 @@
 function toggleMenu() {
-  const menu = document.getElementById('mobileMenu');
-  menu.classList.toggle('active');
-}
-
-// scriptHeader.js
-function toggleMenu() {
-  const m = document.getElementById("mobileMenu");
-  if (!m) return;
-  m.classList.toggle("open");
+  const menu = document.getElementById("mobileMenu");
+  if (!menu) return;
+  menu.classList.toggle("active");
 }
